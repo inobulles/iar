d3c406a688f0c09df4bf8f78ac7cec8cd6ebc51a
obiwac <obiwac@gmail.com>
2020-12-31 16:30:56 +0100

    Made build script a bit more platform agnostic

--- build.sh
+++ build.sh
@@ -2,9 +2,9 @@
 set -e
 
 echo "[IAR Builder] Compiling source ..."
-gcc -g main.c -o iar
+cc main.c -o iar
 
 echo "[IAR Builder] Installing binary ..."
-sudo mv iar /usr/local/bin
+su -l root -c "mv `realpath iar` /usr/local/bin"
 
 exit 0
