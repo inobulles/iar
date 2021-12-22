1d4a3c50360a320e91da897c261e37058bb29905
obiwac <obiwac@gmail.com>
2021-12-22 10:18:31 +0100

    src: bit of cleanup

--- src/main.c
+++ src/main.c
@@ -31,51 +31,62 @@ int main(int argc, char** argv) {
 	char* pack_output = "output.iar";
 	char* unpack_output = "output";
 	
-	char* unpack_file = (char*) 0;
-	char* pack_directory = (char*) 0;
+	char* unpack_file = NULL;
+	char* pack_directory = NULL;
 	
 	for (int i = 1; i < argc; i++) {
-		if (strncmp(argv[i], "--", 2) == 0) { // argument is option
-			char* option = argv[i] + 2;
+		if (strncmp(argv[i], "--", 2)) {
+			fprintf(stderr, "ERROR Unexpected argument '%s'\n", argv[i]);
+			return 1;
+		}
+
+		char* option = argv[i] + 2;
+		
+		if (strcmp(option, "align") == 0) {
+			page_bytes = atoll(argv[++i]);
 			
-			if (strcmp(option, "align") == 0) {
-				if ((page_bytes = atoll(argv[++i])) < 1) {
-					fprintf(stderr, "ERROR Provided page size (%lu) is too small\n", page_bytes);
-					return 1;
-				}
-				
-			} else if (strcmp(option, "version") == 0) {
-				printf("Command-line utility supports up to IAR version %lu\n", IAR_VERSION);
-				return 0;
-				
-			} else if (strcmp(option, "output") == 0) {
-				pack_output = unpack_output = argv[++i];
-				
-			} else if (strcmp(option, "pack") == 0) {
-				if (mode == MODE_UNPACK) {
-					fprintf(stderr, "ERROR '--unpack' has already been passed\n");
-					return 1;
-				}
-				
-				mode = MODE_PACK;
-				pack_directory = argv[++i];
-				
-			} else if (strcmp(option, "unpack") == 0) {
-				if (mode == MODE_PACK) {
-					fprintf(stderr, "ERROR '--pack' has already been passed\n");
-					return 1;
-				}
-				
-				mode = MODE_UNPACK;
-				unpack_file = argv[++i];
-				
-			} else {
-				fprintf(stderr, "ERROR Option '--%s' is unknown. Check README.md or go to https://github.com/inobulles/iar/blob/master/README.md to see a list of available options\n", option);
+			if (page_bytes < 1) {
+				fprintf(stderr, "ERROR Provided page size (%lu) is too small\n", page_bytes);
 				return 1;
 			}
 			
-		} else {
-			fprintf(stderr, "ERROR Unexpected argument '%s'\n", argv[i]);
+		}
+		
+		else if (strcmp(option, "version") == 0) {
+			printf("Command-line utility supports up to IAR version %lu\n", IAR_VERSION);
+			return 0;
+			
+		}
+		
+		else if (strcmp(option, "output") == 0) {
+			pack_output = unpack_output = argv[++i];
+			
+		}
+		
+		else if (strcmp(option, "pack") == 0) {
+			if (mode == MODE_UNPACK) {
+				fprintf(stderr, "ERROR '--unpack' has already been passed\n");
+				return 1;
+			}
+			
+			mode = MODE_PACK;
+			pack_directory = argv[++i];
+			
+		}
+		
+		else if (strcmp(option, "unpack") == 0) {
+			if (mode == MODE_PACK) {
+				fprintf(stderr, "ERROR '--pack' has already been passed\n");
+				return 1;
+			}
+			
+			mode = MODE_UNPACK;
+			unpack_file = argv[++i];
+			
+		}
+		
+		else {
+			fprintf(stderr, "ERROR Option '--%s' is unknown. Check README.md or go to https://github.com/inobulles/iar/blob/master/README.md to see a list of available options\n", option);
 			return 1;
 		}
 	}
@@ -97,7 +108,9 @@ int main(int argc, char** argv) {
 		iar_write_header(&iar);
 		iar_close(&iar);
 		
-	} else if (mode == MODE_UNPACK) {
+	}
+	
+	else if (mode == MODE_UNPACK) {
 		if (iar_open_read(&iar, unpack_file)) {
 			return 1;
 		}
